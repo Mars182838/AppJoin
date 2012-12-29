d5264248c17d67737018cd5606c6e3c553ff8e7d
Mars182838 <wj182838@126.com>
2012-12-29 11:09:56 +0800

    Initial Commit

---
+++ AppJoin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppJoin
+//
+//  Created by Mars on 12-12-29.
+//  Copyright (c) 2012年 Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppJoinTests/AppJoinTests.m
@@ -0,0 +1,32 @@
+//
+//  AppJoinTests.m
+//  AppJoinTests
+//
+//  Created by Mars on 12-12-29.
+//  Copyright (c) 2012年 Mars. All rights reserved.
+//
+
+#import "AppJoinTests.h"
+
+@implementation AppJoinTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AppJoinTests");
+}
+
+@end
